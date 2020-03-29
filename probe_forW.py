5896b030b19b0c5a4af1556eb35cad4b8683b218
双葉屋ほいる <62800179+hoiru-futabaya@users.noreply.github.com>
2020-03-29 23:32:50 +0900

    Update and rename probe.py to probe_forW.py
    line10 対象ファイルの権限を変更させた

--- probe_forW.py
+++ probe_forW.py
@@ -1,13 +1,21 @@
 import ffmpeg
 import json
+import sys
+import os
 
-path = "/storage/761C-FA25/DCIM/101INCAM/191217_165243.mp4"
+sys.path.append(r"D:\aquar\python2\Lib\site-packages\ffmpeg\ffmpeg-20200324-e5d25d1-win32-static\ffmpeg-20200324-e5d25d1-win32-static")
+
+path = r"D:\aquar\Pictures\002.mp4"
+
+os.chmod(path, 0777)
 
 info = ffmpeg.probe(path)
 
 rotate = next((stream for stream in info['streams'] if stream['codec_type'] == 'video'), None)
 
 print(rotate["tags"]["rotate"])
+
+#print(json.dumps(info, indent = 2))
 
 input = ffmpeg.input(path)
 
