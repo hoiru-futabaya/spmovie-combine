343ca957531370ae2993f797589d357cb9bd20ab
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-04-01 23:39:09 +0900

    因数分からん

--- transpose.py
+++ transpose.py
@@ -12,8 +12,8 @@ input = ffmpeg.input(path)
 audio = input.audio
 
 PAD_OPTIONS={'width':'2000','height':'2000','padright':'606','padleft':'606','color':'Black'}
-#input = input.filter('transpose', 1)
-input = input.filter('scale')
+input = input.filter('transpose', 1)
+input = input.filter('scale', 608, -1)
 #input = input.filter('pad',PAD_OPTIONS)
 
 out =ffmpeg.output(input, audio, 'test.mp4')
