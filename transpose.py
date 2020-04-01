0c4816210ef754207775c15a2aa8b505c818b65b
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-04-01 22:30:35 +0900

    scaleとpadがうまくいかない

--- transpose.py
+++ transpose.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python
+# coding: utf8
 import ffmpeg
 import os
 
 path = '001.mp4'
 
-os.chmod(path, 0777)
+#os.chmod(path, 0777)
 
 input = ffmpeg.input(path)
 
-input = input.filter('transpose', 1)
+audio = input.audio
 
-out =ffmpeg.output(input, 'test.mp4')
+PAD_OPTIONS={'width':'2000','height':'2000','padright':'606','padleft':'606','color':'Black'}
+#input = input.filter('transpose', 1)
+input = input.filter('scale')
+#input = input.filter('pad',PAD_OPTIONS)
+
+out =ffmpeg.output(input, audio, 'test.mp4')
 
 ffmpeg.run(out)
